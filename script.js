8c89c82611905a9932324f0f490d0d4e1452bc84
No0o8ster <smartrls46@gmail.com>
2022-10-31 12:56:18 +0200

    Edit index.html And style.css And script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const ROCK = 'rock'
+const PAPER = 'paper'
+const SCISSORS = 'scissors'
+const option = [ROCK, PAPER, SCISSORS]
+const output = document.querySelector('#output')
 
+const rockBtn = document.querySelector('#rockBtn');
+rockBtn.addEventListener('click', () => {output.innerHTML = rules(computerOutput(), ROCK)}
+);
+
+const paperBtn = document.querySelector('#paperBtn');
+paperBtn.addEventListener('click', () => {output.innerHTML = rules(computerOutput(), PAPER)});
+
+const scissorsBtn = document.querySelector('#scissorsBtn');
+scissorsBtn.addEventListener('click', () => {output.innerHTML = rules(computerOutput(), SCISSORS)}
+);
+function computerOutput(){
+    return option[Math.floor(Math.random()* option.length)]
+}
+
+function rules(computer, user){
+    if ((user === ROCK && computer == SCISSORS) || 
+    (user === PAPER && computer === ROCK) ||
+    (user === SCISSORS && computer === PAPER)
+    ){return 'user'}
+    if (user === computer){
+        return 'tie'
+    }
+
+    else {return 'computer'}
+}
